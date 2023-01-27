e07f12bcc9b74bef1496d0d8228a75d61429f087
Denwa799 <denwa799@yandex.ru>
2023-01-27 18:13:59 +0300

    Добавляет чекбокс

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,11 +1,14 @@
 import { FC } from 'react';
 import { AppContainer } from 'layouts/AppContainer';
 import { AppHeadBar } from 'components/AppHeadBar';
+import { AppCheckbox } from 'UI/AppCheckbox';
+import { FormControlLabel } from '@mui/material';
 
 export const MainPage: FC = () => {
   return (
     <AppContainer>
       <AppHeadBar title="To Do" />
+      <FormControlLabel control={<AppCheckbox />} label="Today Tasks:" />
     </AppContainer>
   );
 };
