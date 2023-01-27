e187fbb877b774034450abefe4f6a687db1badb5
Denwa799 <denwa799@yandex.ru>
2023-01-27 15:32:27 +0300

    Добавляет переменные цветов

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import { MainPage } from 'pages/Main';
+import './App.css';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
 };
 
 export default App;
